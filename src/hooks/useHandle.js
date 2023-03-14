20feb3584e17d40558295d87f6d09a880070cdeb
TorinGitIBM <torinmccallum@gmail.com>
2023-03-13 21:10:40 -0400

    create hook for handle

---
+++ src/hooks/useHandle.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+function useHandle(solution) {
+  const [turn, setTurn] = useState(0); //guess count user is on (1-6)
+  const [current, setCurrent] = useState(""); //what is currently being entered
+  const [guessList, setGuessList] = useState([]); //list of previous guesses as arrays
+  const [history, setHistory] = useState([]); //list of previous guesses as strings
+  const [isCorrect, setCorrect] = useState(false); //true when user wins game
+
+  function formatGuess() {}
+  function addNewGuess() {}
+  function handleKeyup() {}
+
+  return { turn, current, guessList, isCorrect, handleKeyup };
+}
+export default useHandle;
