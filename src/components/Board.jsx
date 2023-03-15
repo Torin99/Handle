937a7cf4fa26e0977f02142dee9d1ceb39ab5c91
TorinGitIBM <torinmccallum@gmail.com>
2023-03-15 16:30:19 -0400

    move board components to components dir

---
+++ src/components/Board.jsx
@@ -0,0 +1,17 @@
+import BoardRow from "./BoardRow";
+function Board({ entry, guessList, solution }) {
+  let word = (entry + "     ").substring(0, 5).split("");
+  return (
+    <div className="Board">
+      {guessList.map((guess) => (
+        <BoardRow
+          key={guessList.indexOf(guess)}
+          entry={guess}
+          solution={solution}
+        />
+      ))}
+      <BoardRow entry={word} solution={solution} />
+    </div>
+  );
+}
+export default Board;
